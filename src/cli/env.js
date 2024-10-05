cdabd36779f64a96d753047872a55f8735b5ce1b
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-05 15:47:49 +0200

    feat: parse environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const eVariables = { ...process.env };
+  const result = [];
+
+  for (let key in eVariables) {
+    if (key.startsWith("RSS_")) result.push(`${key}=${eVariables[key]}`);
+  }
+  console.log(result.length ? result.join("; ") : "No RSS variables");
 };
 
 parseEnv();
