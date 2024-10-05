8b945dceec93c2cf8d18bc2f50c3864a1c493025
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-05 15:36:30 +0200

    feat: parse arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const temp = [...process.argv];
+
+  let result = [];
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i].startsWith("--")) result.push(`${temp[i]} is ${temp[i + 1] || null}`);
+  }
+
+  console.log(result.length ? result.join(", ") : 'No arguments were passed');
 };
 
 parseArgs();
