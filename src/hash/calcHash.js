676f987a5e807649f136ae0f0ea05a59649e2555
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-05 18:09:07 +0200

    feat: file hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+const { createHash } = await import("node:crypto");
+import path from "path";
+import { readFile } from "fs/promises";
+import { createReadStream } from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToFile = path.resolve(
+    import.meta.dirname,
+    "./files/fileToCalculateHashFor.txt"
+  );
+
+  const hash = createHash("sha256");
+
+  const rs = createReadStream(pathToFile);
+  rs.on("error", (err) => console.error("Failed to read file", err));
+
+  rs.pipe(hash);
+
+  hash.on("finish", () => {
+    const fileHash = hash.digest("hex");
+    console.log(fileHash);
+  });
 };
 
 await calculateHash();
