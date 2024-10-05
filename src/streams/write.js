1cdc95f44b737b21e8fdd0ee599b122d5113ff88
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-05 20:07:27 +0200

    feat: write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from "node:fs";
+import path from "node:path";
+import { pipeline } from "node:stream/promises";
+import { stdin } from "node:process";
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToFile = path.resolve(
+    import.meta.dirname,
+    "./files/fileToWrite.txt"
+  );
+
+  console.log(
+    "Enter the text. Once completed, press Ctrl+C to exit."
+  );
+  await pipeline(stdin, createWriteStream(pathToFile));
 };
 
 await write();
