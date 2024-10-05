45927aa03216d59a3611d7b6243a50c1caffce4f
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-05 13:39:13 +0200

    feat: copy files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,49 @@
+import { readdir, mkdir, copyFile, access } from "fs/promises";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToDir = path.resolve(import.meta.dirname, "./files");
+  const copyDirPath = path.resolve(import.meta.dirname, "./files_copy");
+
+  const checkFilesDir = async () => {
+    try {
+      await access(pathToDir);
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        throw new Error("FS operation failed: " + error.message);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  const createCopyDir = async () => {    
+    try {
+      await mkdir(copyDirPath);
+    } catch (error) {
+      if (error.code === "EEXIST") {
+        throw new Error("FS operation failed: " + error.message);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  try {
+    await checkFilesDir();
+    await createCopyDir();
+
+    const dir = await readdir(pathToDir, { recursive: true });
+
+    dir.forEach(async (file) => {
+      const pathToFile = path.resolve(pathToDir, file);
+      const copyPathToFile = path.resolve(copyDirPath, file);
+      await copyFile(pathToFile, copyPathToFile);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await copy();
