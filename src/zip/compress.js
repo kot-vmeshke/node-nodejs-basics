afa1cd9475780992e8ffb994d86287c67181b8a5
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-06 18:51:18 +0200

    feat: compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { pipeline } from "node:stream/promises";
+import path from "node:path";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToFile = path.resolve(
+    import.meta.dirname,
+    "./files/fileToCompress.txt"
+  );
+
+  const pathToArchive = path.resolve(import.meta.dirname, "./files/archive.gz");
+
+  const gzip = createGzip();
+
+  await pipeline(
+    createReadStream(pathToFile),
+    gzip,
+    createWriteStream(pathToArchive)
+  );
 };
 
 await compress();
