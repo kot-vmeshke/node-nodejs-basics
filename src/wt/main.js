3e3870de92ced67000209b54c402eb251f246448
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-07 20:57:10 +0200

    feat: workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { isMainThread, Worker } from "worker_threads";
+import path from "path";
+import os from "os";
+
+const NUM = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const pathToWorker = path.resolve(import.meta.dirname, "./worker.js");
+  const threadsAvailable = os.cpus().length;
+
+  const result = [];
+
+  for (let i = 0; i < threadsAvailable; i++) {
+    const worker = new Worker(pathToWorker, { workerData: { num: NUM + i } });
+
+    worker.on("message", (data) => {
+      result.push({ status: "resolved", data });
+    });
+    worker.on("error", () => {
+      result.push({ status: "error", data: null });
+    });
+  }
+
+  const interval = setInterval(() => {
+    if (result.length === threadsAvailable) {
+      console.log(result);
+      clearInterval(interval);
+    }
+  }, 4);
 };
 
 await performCalculations();
