d8ef39a282d23a0eaf1a6404ed5a187f6b361fff
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-07 21:40:09 +0200

    feat: create child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from "node:child_process";
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  const pathToScript = path.resolve(import.meta.dirname, "./files/script.js");
+  const children = spawn(process.argv[0], [pathToScript, ...args]);
+
+  children.stdin.on("data", (data) => {
+    console.log(data);
+  });
+  children.stdout.on("data", (data) => {
+    console.log(data);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
